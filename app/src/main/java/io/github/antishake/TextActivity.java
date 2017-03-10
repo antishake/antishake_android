0a4503b3026ad5de1816db849ef86b9d1203c4cd
Ruraj Joshi <Ruraj Joshi>
2017-03-09 23:52:56 -0600

    Added Android project files and blank activities for text, video, settings and main. Added .editorconfig file for formatting consistency. (Dummy test cases included).

---
+++ app/src/main/java/io/github/antishake/TextActivity.java
@@ -0,0 +1,13 @@
+package io.github.antishake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_text);
+  }
+}
